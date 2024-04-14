58a6bf3110b95b493a50c87dea1330808c277b96
Joseph Villamar <joseph_242_41@hotmail.com>
2024-04-13 22:04:45 -0500

    testing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def weather():
         city = request.form['city']
     else:
         #for default name arequipa
-        city = 'arequipa'
+        city = 'lima'
 
     # source contain json data from api
     try:
